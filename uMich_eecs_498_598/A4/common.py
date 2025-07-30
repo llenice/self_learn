55ef23b7cb0fd386630ce1e8181f6bc4798d032d
llenice <hjh2224242069@163.com>
2025-07-30 12:15:38 +0800

    add A4

---
+++ uMich_eecs_498_598/A4/common.py
@@ -0,0 +1,293 @@
+"""
+This module contains classes and functions that are common across both, one-stage
+and two-stage detector implementations. You have to implement some parts here -
+walk through the notebooks and you will find instructions on *when* to implement
+*what* in this module.
+"""
+from typing import Dict, Tuple
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torchvision import models
+from torchvision.models import feature_extraction
+
+
+def hello_common():
+    print("Hello from common.py!")
+
+
+class DetectorBackboneWithFPN(nn.Module):
+    r"""
+    Detection backbone network: A tiny RegNet model coupled with a Feature
+    Pyramid Network (FPN). This model takes in batches of input images with
+    shape `(B, 3, H, W)` and gives features from three different FPN levels
+    with shapes and total strides upto that level:
+
+        - level p3: (out_channels, H /  8, W /  8)      stride =  8
+        - level p4: (out_channels, H / 16, W / 16)      stride = 16
+        - level p5: (out_channels, H / 32, W / 32)      stride = 32
+
+    NOTE: We could use any convolutional network architecture that progressively
+    downsamples the input image and couple it with FPN. We use a small enough
+    backbone that can work with Colab GPU and get decent enough performance.
+    """
+
+    def __init__(self, out_channels: int):
+        super().__init__()
+        self.out_channels = out_channels
+
+        # Initialize with ImageNet pre-trained weights.
+        _cnn = models.regnet_x_400mf(pretrained=True)
+
+        # Torchvision models only return features from the last level. Detector
+        # backbones (with FPN) require intermediate features of different scales.
+        # So we wrap the ConvNet with torchvision's feature extractor. Here we
+        # will get output features with names (c3, c4, c5) with same stride as
+        # (p3, p4, p5) described above.
+        self.backbone = feature_extraction.create_feature_extractor(
+            _cnn,
+            return_nodes={
+                "trunk_output.block2": "c3",
+                "trunk_output.block3": "c4",
+                "trunk_output.block4": "c5",
+            },
+        )
+
+        # Pass a dummy batch of input images to infer shapes of (c3, c4, c5).
+        # Features are a dictionary with keys as defined above. Values are
+        # batches of tensors in NCHW format, that give intermediate features
+        # from the backbone network.
+        dummy_out = self.backbone(torch.randn(2, 3, 224, 224))
+        dummy_out_shapes = [(key, value.shape) for key, value in dummy_out.items()]
+
+        print("For dummy input images with shape: (2, 3, 224, 224)")
+        for level_name, feature_shape in dummy_out_shapes:
+            print(f"Shape of {level_name} features: {feature_shape}")
+
+        ######################################################################
+        # TODO: Initialize additional Conv layers for FPN.                   #
+        #                                                                    #
+        # Create THREE "lateral" 1x1 conv layers to transform (c3, c4, c5)   #
+        # such that they all end up with the same `out_channels`.            #
+        # Then create THREE "output" 3x3 conv layers to transform the merged #
+        # FPN features to output (p3, p4, p5) features.                      #
+        # All conv layers must have stride=1 and padding such that features  #
+        # do not get downsampled due to 3x3 convs.                           #
+        #                                                                    #
+        # HINT: You have to use `dummy_out_shapes` defined above to decide   #
+        # the input/output channels of these layers.                         #
+        ######################################################################
+        # This behaves like a Python dict, but makes PyTorch understand that
+        # there are trainable weights inside it.
+        # Add THREE lateral 1x1 conv and THREE output 3x3 conv layers.
+        self.fpn_params = nn.ModuleDict()
+
+        # Replace "pass" statement with your code
+        # 1. 创建横向连接层 (1x1 卷积)
+        # dummy_out_shapes[0] 是 ("c3", shape)
+        self.fpn_params["lateral_c3"] = nn.Conv2d(dummy_out_shapes[0][1][1], self.out_channels, 1)
+        self.fpn_params["lateral_c4"] = nn.Conv2d(dummy_out_shapes[1][1][1], self.out_channels, 1)
+        self.fpn_params["lateral_c5"] = nn.Conv2d(dummy_out_shapes[2][1][1], self.out_channels, 1)
+
+        # 2. 创建输出层 (3x3 卷积)
+        for i in range(3, 6):
+            self.fpn_params[f"output{i}"] = nn.Conv2d(self.out_channels, self.out_channels, 
+                                                      kernel_size=3, padding=1) #padding=1 确保 3x3 卷积核不会改变特征图的空间尺寸
+                                                  
+        ######################################################################
+        #                            END OF YOUR CODE                        #
+        ######################################################################
+
+    @property
+    def fpn_strides(self):
+        """
+        Total stride up to the FPN level. For a fixed ConvNet, these values
+        are invariant to input image size. You may access these values freely
+        to implement your logic in FCOS / Faster R-CNN.
+        """
+        return {"p3": 8, "p4": 16, "p5": 32}
+
+    def forward(self, images: torch.Tensor):
+
+        # Multi-scale features, dictionary with keys: {"c3", "c4", "c5"}.
+        backbone_feats = self.backbone(images)
+
+        fpn_feats = {"p3": None, "p4": None, "p5": None}
+        ######################################################################
+        # TODO: Fill output FPN features (p3, p4, p5) using RegNet features  #
+        # (c3, c4, c5) and FPN conv layers created above.                    #
+        # HINT: Use `F.interpolate` to upsample FPN features.                #
+        ######################################################################
+
+        # Replace "pass" statement with your code
+        c3, c4, c5 = backbone_feats["c3"], backbone_feats["c4"], backbone_feats["c5"]
+
+        l3 = self.fpn_params["lateral_c3"](c3)
+        l4 = self.fpn_params["lateral_c4"](c4)
+        l5 = self.fpn_params["lateral_c5"](c5)
+
+        p5 = self.fpn_params["output5"](l5)
+        # p5 * 2，与l4尺寸对齐
+        p4_interim = F.interpolate(p5, scale_factor=2, mode="bilinear") + l4
+        p4 = self.fpn_params["output4"](p4_interim)
+        p3_interim = F.interpolate(p4, scale_factor=2, mode="bilinear") + l3
+        p3 = self.fpn_params["output3"](p3_interim)
+        
+        fpn_feats = {"p3": p3, "p4": p4, "p5": p5}
+        ######################################################################
+        #                            END OF YOUR CODE                        #
+        ######################################################################
+
+        return fpn_feats
+
+
+def get_fpn_location_coords(
+    shape_per_fpn_level: Dict[str, Tuple],
+    strides_per_fpn_level: Dict[str, int],
+    dtype: torch.dtype = torch.float32,
+    device: str = "cpu",
+) -> Dict[str, torch.Tensor]:
+    """
+    Map every location in FPN feature map to a point on the image. This point
+    represents the center of the receptive field of this location. We need to
+    do this for having a uniform co-ordinate representation of all the locations
+    across FPN levels, and GT boxes.
+
+    Args:
+        shape_per_fpn_level: Shape of the FPN feature level, dictionary of keys
+            {"p3", "p4", "p5"} and feature shapes `(B, C, H, W)` as values.
+        strides_per_fpn_level: Dictionary of same keys as above, each with an
+            integer value giving the stride of corresponding FPN level.
+            See `backbone.py` for more details.
+
+    Returns:
+        Dict[str, torch.Tensor]
+            Dictionary with same keys as `shape_per_fpn_level` and values as
+            tensors of shape `(H * W, 2)` giving `(xc, yc)` co-ordinates of the
+            centers of receptive fields of the FPN locations, on input image.
+    """
+
+    # Set these to `(N, 2)` Tensors giving absolute location co-ordinates.
+    location_coords = {
+        level_name: None for level_name, _ in shape_per_fpn_level.items()
+    }
+
+    for level_name, feat_shape in shape_per_fpn_level.items():
+        level_stride = strides_per_fpn_level[level_name]
+
+        ######################################################################
+        # TODO: Implement logic to get location co-ordinates below.          #
+        ######################################################################
+        # Replace "pass" statement with your code
+        B, C, H, W = feat_shape
+
+        # 创建网格坐标
+        y_coords = torch.arange(H, dtype=dtype, device=device)
+        x_coords = torch.arange(W, dtype=dtype, device=device)
+
+        shift_y, shift_x = torch.meshgrid(y_coords, x_coords, indexing="ij")
+
+        # 将特征图坐标转换为输入图像坐标
+        image_x = level_stride * (shift_x + 0.5)
+        image_y = level_stride * (shift_y + 0.5)
+
+        # 将坐标重塑为 (H*W, 2) 的格式,  每一行是 [x_coord, y_coord]
+        # dim = 1 , 堆叠成 (H*W, 2) 的张量，每行是 [x, y]
+        location_coords[level_name] = torch.stack([image_y.flatten(), image_x.flatten()], dim=1)
+        ######################################################################
+        #                             END OF YOUR CODE                       #
+        ######################################################################
+    return location_coords
+
+
+def nms(boxes: torch.Tensor, scores: torch.Tensor, iou_threshold: float = 0.5):
+    """
+    Non-maximum suppression removes overlapping bounding boxes.
+
+    Args:
+        boxes: Tensor of shape (N, 4) giving top-left and bottom-right coordinates
+            of the bounding boxes to perform NMS on.
+        scores: Tensor of shpe (N, ) giving scores for each of the boxes.
+        iou_threshold: Discard all overlapping boxes with IoU > iou_threshold
+
+    Returns:
+        keep: torch.long tensor with the indices of the elements that have been
+            kept by NMS, sorted in decreasing order of scores;
+            of shape [num_kept_boxes]
+    """
+
+    if (not boxes.numel()) or (not scores.numel()):
+        return torch.zeros(0, dtype=torch.long)
+
+    keep = None
+    #############################################################################
+    # TODO: Implement non-maximum suppression which iterates the following:     #
+    #       1. Select the highest-scoring box among the remaining ones,         #
+    #          which has not been chosen in this step before                    #
+    #       2. Eliminate boxes with IoU > threshold                             #
+    #       3. If any boxes remain, GOTO 1                                      #
+    #       Your implementation should not depend on a specific device type;    #
+    #       you can use the device of the input if necessary.                   #
+    # HINT: You can refer to the torchvision library code:                      #
+    # github.com/pytorch/vision/blob/main/torchvision/csrc/ops/cpu/nms_kernel.cpp
+    #############################################################################
+    # Replace "pass" statement with your code
+    x1, y1, x2, y2 = boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3]
+    areas = (x2 - x1) * (y2 -y1)
+    keep = []
+    order = scores.argsort(descending=True) #tensor
+    while order.numel() > 0:
+        max_idx = order[0]
+        keep.append(max_idx.item())
+        
+        if order.numel() == 1:
+            break
+        rest = order[1:]
+
+        # 计算交集区域的坐标
+        inter_x1 = torch.maximum(x1[max_idx], x1[rest])
+        inter_y1 = torch.maximum(y1[max_idx], y1[rest])
+        inter_x2 = torch.minimum(x2[max_idx], x2[rest])
+        inter_y2 = torch.minimum(y2[max_idx], y2[rest])
+
+        w = torch.clamp(inter_x2 - inter_x1, 0.0)
+        h = torch.clamp(inter_y2 - inter_y1, 0.0)
+
+        intersection = h * w
+        union = areas[max_idx] + areas[rest] -intersection
+        iou = intersection / union
+
+        keep_mask = iou <= iou_threshold
+        order = order[1:][keep_mask]
+
+        
+
+    #############################################################################
+    #                              END OF YOUR CODE                             #
+    #############################################################################
+    return torch.tensor(keep)
+
+
+def class_spec_nms(
+    boxes: torch.Tensor,
+    scores: torch.Tensor,
+    class_ids: torch.Tensor,
+    iou_threshold: float = 0.5,
+):
+    """
+    Wrap `nms` to make it class-specific. Pass class IDs as `class_ids`.
+    STUDENT: This depends on your `nms` implementation.
+
+    Returns:
+        keep: torch.long tensor with the indices of the elements that have been
+            kept by NMS, sorted in decreasing order of scores;
+            of shape [num_kept_boxes]
+    """
+    if boxes.numel() == 0:
+        return torch.empty((0,), dtype=torch.int64, device=boxes.device)
+    max_coordinate = boxes.max()
+    offsets = class_ids.to(boxes) * (max_coordinate + torch.tensor(1).to(boxes))
+    boxes_for_nms = boxes + offsets[:, None]
+    keep = nms(boxes_for_nms, scores, iou_threshold)
+    return keep
